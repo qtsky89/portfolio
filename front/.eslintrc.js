8102e924ba02831335fe1e558d9053e574a0f0b0
Qtsky89 <Qtsky89@gmail.com>
2020-12-20 18:03:20 +0900

    [#1] Added config for vue

--- front/.eslintrc.js
+++ front/.eslintrc.js
@@ -7,8 +7,6 @@ module.exports = {
     "plugin:vue/essential",
     "eslint:recommended",
     "@vue/typescript/recommended",
-    "@vue/prettier",
-    "@vue/prettier/@typescript-eslint"
   ],
   parserOptions: {
     ecmaVersion: 2020
@@ -16,6 +14,7 @@ module.exports = {
   rules: {
     "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
     "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off"
+    '@typescript-eslint/no-var-requires': 0,
   },
   overrides: [
     {
