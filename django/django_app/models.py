a873ec16fedf95b638524357d05aaa7ef02ca03d
Qtsky89 <Qtsky89@gmail.com>
2021-02-14 01:18:46 +0900

    [#1] Added model

--- django/django_app/models.py
+++ django/django_app/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class Project(models.Model):
+    name = models.CharField(max_length=256, unique=True, primary_key=True)
+    image = models.CharField(max_length=4096, default='')
+    constraint = models.CharField(max_length=256, default='')
+    created_date = models.DateTimeField(default=timezone.now)
+    def __str__(self):
+        return str(self.service_name)
