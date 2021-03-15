0cc4e68df01c1998c9fb603a7f0c594ff1abc0f1
Qtsky89 <Qtsky89@gmail.com>
2021-03-15 22:09:11 +0900

    [#3] Polished Dockerfile

--- django/django_project/settings.py
+++ django/django_project/settings.py
@@ -39,7 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django_app',
-    'corsheaders'
+    'corsheaders',
+    'mod_wsgi.server'
 ]
 
 MIDDLEWARE = [
