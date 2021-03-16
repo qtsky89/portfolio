59187d5e547dc2eec813cf1cfc912cfb826b0382
Qtsky89 <Qtsky89@gmail.com>
2021-03-16 15:53:30 +0900

    [#3] Added virtualhost config

--- django/django_project/settings.py
+++ django/django_project/settings.py
@@ -39,8 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django_app',
-    'corsheaders',
-    'mod_wsgi.server'
+    'corsheaders'
 ]
 
 MIDDLEWARE = [
